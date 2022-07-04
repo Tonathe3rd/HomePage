8c10c427b72d046d7606ee778088eab6426093f9
Tonathe3rd <get2adeshola@gmail.com>
2022-07-04 15:42:38 +0100

    changed index file name

--- little.js
+++ little.js
@@ -1,19 +1,49 @@
-var yournam = document.getElementById("name");
-var email = document.getElementById("email");
-var message = document.getElementById("message");
-var submit = document.getElementById("submit");
-var boxName = document.getElementById("namer");
-var boxEmail = document.getElementById("emailer");
-var boxMessage = document.getElementById("messager");
+let yournam = document.getElementById("name");
+let email = document.getElementById("email");
+let message = document.getElementById("message");
+let submit = document.getElementById("submit");
+let boxName = document.getElementById("namer");
+let boxEmail = document.getElementById("emailer");
+let boxMessage = document.getElementById("messager");
 
-submit.onclick = function(){
-        boxName.value = "Name: " + yournam.value
-        boxEmail.value = "Email: " + email.value
-        boxMessage.value = "Message: " + message.value
-        yournam.value = ""
-        email.value = ""
-        message.value = ""
-        submit.innerText = "Done"
+// submit.onclick = function(){
+//     boxName.value = "Name: " + yournam.value
+//     boxEmail.value = "Email: " + email.value
+//     boxMessage.value = "Message: " + message.value
+//     yournam.value = ""
+//     email.value = ""
+//     message.value = ""
+//     submit.innerText = "Done" 
+//     // submit.style.backgroundColor = "red"
+// }
+
+function submitForm() {
+    submit.onclick = function(){
+       boxName.value = "Name: " + yournam.value
+       boxEmail.value = "Email: " + email.value
+       boxMessage.value = "Message: " + message.value
+       yournam.value = ""
+       email.value = ""
+       message.value = ""
+       submit.innerText = "Done" 
+       // submit.style.backgroundColor = "red"
     }
+    return true
+}
+submitForm();
 
-// submit.style.backgroundColor = "black";
+// setTimeout(() => {
+//     if (submitForm() === true) {
+//         submit.innerText = "Submit"
+//     }    
+//     // submit.style.backgroundColor = "red"
+// }, 300);
+
+
+
+//This code is creating page unresponsive
+// setTimeout(() => {
+//     while (submitForm() === true) {
+//         submit.innerText = "Submit"
+//     }
+// }, 300);
